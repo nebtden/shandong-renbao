208c257608d1e2c745d6d73093dd9c5b7946ecea
nebtden <nebtden@google.com>
2019-09-17 09:35:37 +0800

    山东项目更新

--- frontend/views/shandong-renbao-hero/mobile.php
+++ frontend/views/shandong-renbao-hero/mobile.php
@@ -2,10 +2,11 @@
 <div class="getbag-bg">
     <h1 class="hide">猜英雄 赢大奖</h1>
     <div for="" class="getbag-con1">
-        <input type="number" class="vct-number" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 请输入手机号码" >
-        <input type="text"   class="vct-import" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 输入验证码">
+        <input type="number" class="vct-number" placeholder=" 请输入手机号码" >
+        <input type="text"   class="vct-import" placeholder=" 输入验证码">
+        <a href="javascript:;" class="vct-btn"></a>
     </div>
-    <a  href="javascript:popShow('pop_per');" class="scroe-btn submit">
+    <a  href="javascript:;" class="scroe-btn submit">
         领取礼包
     </a>
 
@@ -14,12 +15,13 @@
 <div  class="pop" id="pop_per">
     <div class="pop_cont3">
         <div class="pop-con1">
+            <h2>免费洗车券一张</h2>
             <a href="javascript:;">去使用</a>
             <a href="javascript:popShow('pop1');" >邀朋友一起来PK</a>
             <p>好友获得礼品的同时<br/>
                 将发放一份随机神秘礼给您</p>
         </div>
-        <a href="javascript:;" title="点击关闭" class="btn close gtbcls"> </a>
+        <a href="javascript: popHide();" title="点击关闭" class="btn close gtbcls"> </a>
     </div>
 </div>
 
@@ -30,7 +32,7 @@
 
 <script>
     $(function () {
-        $('.send').click(function () {
+        $('.vct-btn').click(function () {
             var mobile = $('.vct-number').val();
             console.log(mobile);
             //检测手机号码
@@ -64,12 +66,12 @@
                 alert("验证码错误,请检查！");
                 return false;
             }
-
+            popShow('pop_per');
             $.post('submit.html',{mobile:mobile,code:code,license_plate:license_plate},function (data) {
 
                 if(data.status==1){
-                    window.location.href = 'result.html?id='+data.data.id;
-                    popHide();
+                    // window.location.href = 'result.html?id='+data.data.id;
+
 
                 }else{
                     alert(data.message);
